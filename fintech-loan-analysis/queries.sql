852ae01eb1f16a81c3f6b240d66d60e24a08ca01
Bhavana1427 <bhavanabodapati97@gmail.com>
2025-06-12 11:44:27 +0530

    Update queries.sql

--- fintech-loan-analysis/queries.sql
+++ fintech-loan-analysis/queries.sql
@@ -2,3 +2,18 @@
 CREATE TABLE fintech.loan_purposes AS
 SELECT DISTINCT application.purpose
 FROM fintech.loan;
+
+-- Query 1: Total Outstanding Loans
+SELECT SUM(funded_amount) AS total_outstanding_loans
+FROM fintech.loan;
+
+-- Query 2: Count of Loans by Purpose
+SELECT application.purpose, COUNT(*) AS total_loans
+FROM fintech.loan
+GROUP BY application.purpose
+ORDER BY total_loans DESC;
+
+-- Query 3: Average Loan by Grade
+SELECT grade, ROUND(AVG(funded_amount), 2) AS avg_loan
+FROM fintech.loan
+GROUP BY grade;
